2206ccfe333e2e4eff4e8d7ea23f9cc7af6b8439
Diogo Alcarde <diogolimaalcarde@gmail.com>
2022-08-05 15:01:12 -0300

    4

--- pages/api/tempo.js
+++ pages/api/tempo.js
@@ -1,9 +1,14 @@
-function tempo(request, response) {
-
+async function tempo(request, response) {
+  const apiKey = process.env.YOUTUBE_API_KEY;
   const dynamicDate = new Date();
 
+  const kindResponse = await fetch(`https://www.googleapis.com/youtube/v3/search?part=snippet&channelId=UCdlJx-w37viXFNq__qZ7ARQ&order=date&maxResults=20&key=${apiKey}`);
+  const kindResponseJson = await kindResponse.json();
+  const kind = kindResponseJson.kind;
+
   response.json({
-    date: dynamicDate.toDateString()
+    date: dynamicDate.toDateString(),
+    kind: kind
   })
 }
 
